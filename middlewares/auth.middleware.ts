c41be590cfdacb57459a785961f9184305e425f0
Srijan Gupta <gupta.srijan94@gmail.com>
2023-06-21 19:51:35 +0530

    refactor: auth middleware

--- middlewares/auth.middleware.ts
+++ middlewares/auth.middleware.ts
@@ -9,16 +9,14 @@ export const protect = (
   const bearer: string = req.headers.authorization
 
   if (!bearer) {
-    res.status(401)
-    res.json({ message: 'not authorized' })
+    res.status(401).json({ message: 'not authorized' })
     return
   }
 
   const [, token] = bearer.split(' ')
 
   if (!token) {
-    res.status(401)
-    res.json({ message: 'not valid token' })
+    res.status(401).json({ message: 'not valid token' })
     return
   }
 
@@ -28,8 +26,7 @@ export const protect = (
     next()
   } catch (e) {
     console.error(e)
-    res.status(401)
-    res.json({ message: 'not valid token' })
+    res.status(401).json({ message: 'not valid token' })
     return
   }
 }
